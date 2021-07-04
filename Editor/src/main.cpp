573b4ef7efb60fe11067ef6ada4fbed316995cc9
Uğur Güney <ugurguney@gmail.com>
2021-07-04 18:40:33 -0400

    Init usage of ImGui in the project
    * Include headers
    * Create context
    * choose style
    * Init, new frame, render, shutdown OpenGL3 and GLFW implementations
    * give DeltaTime and DisplaySize to ImGuiIO

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -4,6 +4,9 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 #include "glad/glad.h" // All headers will be included by glad
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -23,14 +26,46 @@ int main() {
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
+    io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+    float time = 1.0f / 60.f;
     while (!glfwWindowShouldClose(window))
     {      
         glClearColor((GLfloat)fmod(glfwGetTime(), 1.0), 1.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float curr_time = (float)glfwGetTime();
+        io.DeltaTime = curr_time - time;
+        time = curr_time;
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        io.DisplaySize = ImVec2(width, height);
+
+        // OpenGL drawing commands here?
+
+        ImGui::Begin("My name is window, ImGui window");
+        ImGui::Text("Hello There!");
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
