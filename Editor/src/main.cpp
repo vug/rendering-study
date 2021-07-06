3de45abae1962d8e467461c9124bb3b3290288bc
Uğur Güney <ugurguney@gmail.com>
2021-07-06 19:15:12 -0400

    Use Shader abstraction and simple shaders to colorize Triangle
    triangle from few commits before that was white due to using the default shaders provided by graphics driver.

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -9,6 +9,7 @@
 #include "glm/ext.hpp"
 
 #include "ImGuiLayer.h"
+#include "Shader.h"
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -38,6 +39,31 @@ int main() {
     ImGuiLayer::Init(window);
 
     // OpenGL Prepare
+    std::string vertexSrc = R"(
+        #version 460 core
+
+        layout(location = 0) in vec3 a_Position;
+
+        out vec3 v_Position;        
+
+        void main() {
+            v_Position = a_Position;
+            gl_Position = vec4(a_Position, 1.0);
+        }
+    )";
+    std::string fragmentSrc = R"(
+        #version 460 core
+
+        layout(location = 0) out vec4 color;
+
+        in vec3 v_Position;        
+
+        void main() {
+            color = vec4(v_Position * 0.5 + 0.5, 1.0);
+        }
+    )";
+    std::unique_ptr<Shader> shader;
+    shader.reset(new Shader(vertexSrc, fragmentSrc));
     unsigned int vertexArray, vertexBuffer, indexBuffer;
     // Vertex Array
     glGenVertexArrays(1, &vertexArray);
@@ -80,6 +106,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // OpenGL Render
+        shader->Bind();
         glBindVertexArray(vertexArray);
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
