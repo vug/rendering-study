965961da1d4ee34bc6ffa2785cca2999dbdb66d9
Uğur Güney <ugurguney@gmail.com>
2021-07-09 11:59:36 -0400

    Add more Uniform Upload functions to Shader
    abstracting away more OpenGL code.

--- Editor/src/Renderer/Shader.h
+++ Editor/src/Renderer/Shader.h
@@ -11,7 +11,14 @@ public:
 	void Bind() const;
 	void Unbind() const;
 
+	void UploadUniformInt(const std::string& name, int value);
+	void UploadUniformFloat(const std::string& name, float value);
+	void UploadUniformFloat2(const std::string& name, const glm::vec2& values);
+	void UploadUniformFloat3(const std::string& name, const glm::vec3& values);
 	void UploadUniformFloat4(const std::string& name, const glm::vec4& values);
+
+	void UploadUniformMat2(const std::string& name, const glm::mat2& matrix);
+	void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 	void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 private:
 	uint32_t rendererID;
