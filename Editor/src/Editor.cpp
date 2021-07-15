d9f9be7e92cdf3991f85322d700a57b3d0c9489b
Uğur Güney <ugurguney@gmail.com>
2021-07-15 01:34:24 -0400

    Example hard-coded LineComponent to active scene

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -31,6 +31,12 @@ void Editor::OnInit() {
     auto redSquare = activeScene->CreateEntity("Red Square");
     activeScene->Reg().emplace<QuadRendererComponent>(redSquare, glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
     activeScene->Reg().get<TransformComponent>(redSquare).Translation = glm::vec3{ -0.5f, 0.5f, -0.5f };
+    entt::basic_handle line1{ activeScene->Reg(), activeScene->CreateEntity("Line1") };
+    LineComponent& vc = line1.emplace<LineComponent>(
+        std::vector<glm::vec3>{ {-0.5f, -0.5f, 0.0f}, { 0.4f, -0.4f, 0.0f }, { 0.3f, 0.3f, 0.0f }, { -0.2f, 0.2f, 0.0f } }
+    );
+    line1.get<TransformComponent>().Translation = { 0.0f, 0.0f, 1.0f };
+    
     
     mainCameraEntity = activeScene->CreateEntity("Camera A");
     activeScene->Reg().emplace<CameraComponent>(mainCameraEntity);
