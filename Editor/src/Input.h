caa8fd3fea1d4b647f228a23cf2017108d3d2dca
Uğur Güney <ugurguney@gmail.com>
2021-07-25 16:19:55 -0400

    Input utility functions to query pressed mouse buttons and cursor position.

--- Editor/src/Input.h
+++ Editor/src/Input.h
@@ -9,6 +9,23 @@ public:
 		int keyState = glfwGetKey(Application::Get().GetWindow(), key);
 		return keyState == GLFW_PRESS || keyState == GLFW_REPEAT;
 	}
+
+	static float GetMouseX() {
+		double xPos, yPos;
+		glfwGetCursorPos(Application::Get().GetWindow(), &xPos, &yPos);
+		return (float) xPos;
+	}
+
+	static float GetMouseY() {
+		double xPos, yPos;
+		glfwGetCursorPos(Application::Get().GetWindow(), &xPos, &yPos);
+		return (float)yPos;
+	}
+
+	static bool IsMouseButtonPressed(int button) {
+		int buttonState = glfwGetMouseButton(Application::Get().GetWindow(), button);
+		return buttonState == GLFW_PRESS;
+	}
 };
 
 
