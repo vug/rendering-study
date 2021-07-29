d21e7a8ad3212ae68362bdd0f4d50a7ee43b224c
Uğur Güney <ugurguney@gmail.com>
2021-07-29 06:44:51 -0400

    Add Stanford bunny to mesh assets

--- Editor/src/Renderer/Buffer.h
+++ Editor/src/Renderer/Buffer.h
@@ -50,9 +50,9 @@ struct BufferElement {
 			case ShaderDataType::Float2: return 2;
 			case ShaderDataType::Float3: return 3;
 			case ShaderDataType::Float4: return 4;
-			case ShaderDataType::Mat2:   return 2 * 2;
-			case ShaderDataType::Mat3:   return 3 * 3;
-			case ShaderDataType::Mat4:   return 4 * 4;
+			case ShaderDataType::Mat2:   return 2 * 2; // could be just 2
+			case ShaderDataType::Mat3:   return 3 * 3; // could be just 3
+			case ShaderDataType::Mat4:   return 4 * 4; // could be just 4
 			case ShaderDataType::Int:    return 1;
 			case ShaderDataType::Int2:   return 2;
 			case ShaderDataType::Int3:   return 3;
